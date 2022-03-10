ed5ffe5b690ac99a4c9779d064023432443ae692
sungjeongeon <sje1942@naver.com>
2022-03-10 15:17:03 +0900

    userBar

--- template_airbnb/airbnb.js
+++ template_airbnb/airbnb.js
@@ -10,12 +10,35 @@
 window.onscroll = function() {
 	const topGnb = document.querySelector("#headerTop2");
 	const midGnb = document.querySelector(".headerRes");
-	if (55 > window.scrollY) {
+	if (30 > window.scrollY) {
 		topGnb.style.opacity= "0";
 		midGnb.style.opacity= "1";
 	} else {
 		topGnb.style.top = "0px";
 		topGnb.style.opacity= "1";
 		midGnb.style.opacity= "0";
+	}
+}
+
+/*const topGnb = document.querySelector("#headerTop2");
+const midGnb = document.querySelector(".headerRes");
+window.addEventListener("onscroll", () => {
+	if (30 > window.scrollY) {
+		topGnb.style.opacity= "0";
+		midGnb.style.opacity= "1";
+	} else {
+		topGnb.style.top = "0px";
+		topGnb.style.opacity= "1";
+		midGnb.style.opacity= "0";
+	}
+})*/
+
+window.onload = function() {
+	const user = document.querySelectorAll('.user');
+	const userPage = document.querySelector(".userPage");
+	console.log(user);
+	user[0].onclick = function() {
+		userPage.classList.toggle('active');
+		console.log("테스트");
 	}
 }
